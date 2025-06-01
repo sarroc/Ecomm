a05a4086300a3dd21da909cfb900c49cd3ba437f
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-01 18:08:17 +0200

    find user by id

--- repositories/users.js
+++ repositories/users.js
@@ -38,16 +38,19 @@ class UsersRepository {
   randomId() {
     return crypto.randomBytes(4).toString('hex');
   }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find(record => record.id === id);
+  }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    repo.create({ email: 'test@test.com', password: 'password'});
+    const user = await repo.getOne('');
 
-    const users = await repo.getAll();
-
-    console.log(users);
+    console.log(user);
 };
 
 test();
