27e30080e378794d6a5479b18531b895983819e9
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-01 11:48:01 +0200

    apply middleware globally and change input name to undo string

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
 res.send(`
     <div>
         <form method='POST'>
             <input name='email' placeholder='email' />
             <input name='password' placeholder='password' />
-            <input name='password confirmation' placeholder='password confirmation' />
+            <input name='passwordConfirmation' placeholder='password confirmation' />
             <button>Sign Up</button>
         </form>
     </div>
     `);
 });
 
-app.post('/', bodyParser.urlencoded({ extended: true }), (req, res) => {
+app.post('/', (req, res) => {
     console.log(req.body);
        res.send('Account created!!');
 });
