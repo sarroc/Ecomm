b0c076560320641adf22cf32b251085e218cb033
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-01 18:24:43 +0200

    add function to update records

--- repositories/users.js
+++ repositories/users.js
@@ -49,12 +49,25 @@ class UsersRepository {
     const filteredRecords = records.filter(record => record.id !== id);
     await this.writeAll(filteredRecords);
   }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find(record => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+    
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    await repo.delete('770e754a');
+await repo.update('c66b3f95',{ password: 'mypassword'});
+
 };
 
 test();
