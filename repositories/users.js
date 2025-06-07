ff9aa063fb42386840d0baae7ba2b9c619f2215b
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-07 13:56:26 +0200

    Fix comparePassword function

--- repositories/users.js
+++ repositories/users.js
@@ -47,9 +47,9 @@ class UsersRepository {
     // Saved -> password saved in our database. 'hashed.salt'
     // Supplied -> given to us by a user trying to sign in
     const [hashed, salt] = saved.split('.');
-    const hashedSupplied = await scrypt(supllied, salt, 64);
+    const hashedSuppliedBuf = await scrypt(supllied, salt, 64);
 
-    return hashed === hashedSupplied;
+    return hashed === hashedSuppliedBuf.toString('hex');
   }
 
   async writeAll(records) {
