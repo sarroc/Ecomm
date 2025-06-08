6bfe211d35a2e3cd173d2524459342c01176e7a9
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-08 19:45:46 +0200

    add check function from express-validator library for better form validation and receive validation output

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator')
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -9,7 +11,24 @@ router.get('/signup', (req, res) => {
 res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
+router.post(
+  '/signup', 
+    [
+     check('email')
+        .trim()
+        .normalizeEmail()
+        .isEmail(),
+     check('password')
+        .trim()
+        .isLength({ min:4, max:20 }),
+     check('passwordConfirmation')
+        .trim()
+        .isLength({ min:4, max:20 })
+    ],
+ async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    
     const { email, password, passwordConfirmation } = req.body;
     
     const existingUser = await usersRepo.getOneBy({ email });
