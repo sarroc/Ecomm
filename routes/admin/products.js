38e28debb067e1c123862b1ae2d1a5b5f993c176
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-21 09:43:19 +0200

    add product listings

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -4,13 +4,15 @@ const multer = require('multer');
 const { handleErrors } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
-const {requireTitle, requirePrice } = require('./validators');
+const productsIndexTemplate = require('../../views/admin/products/index');
+const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {
-
+router.get('/admin/products', async (req, res) => {
+    const products = await productsRepo.getAll();
+    res.send(productsIndexTemplate({ products }));
 });
 
 router.get('/admin/products/new', (req, res) => {
@@ -24,7 +26,7 @@ router.post(
     handleErrors(productsNewTemplate),
 async (req, res) => {
     const image = req.file.buffer.toString('base64');
-    const { title , price } = req.body;
+    const { title, price } = req.body;
     await productsRepo.create({ title, price, image });
 
     res.send('submitted');
