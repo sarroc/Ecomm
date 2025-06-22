7c012fd135f5a1657e06d9f5efa54094a563b6a6
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-22 10:34:07 +0200

    add products to cart

--- routes/carts.js
+++ routes/carts.js
@@ -18,9 +18,17 @@ router.post('/cart/products', async (req, res) => {
     cart = await cartsRepo.getOne(req.session.cartId);
     }
     
-    console.log(cart);
-    // Either increment quantity or existing product
-    // OR add new product to items array
+    const existingItem = cart.items.find(item => item.id === req.body.productId);
+    if (existingItem) {
+    // increment quantity and save cart
+       existingItem.quantity++; 
+    } else {
+    // add new product id to items array
+    cart.items.push({ id: req.body.productId, quantity: 1 })
+    }
+    await cartsRepo.update(cart.id, {
+        items: cart.items
+    });
 
     res.send('Product added to cart');
 })
